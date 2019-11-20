800de3df525bd3a5928081913adff950c21c4fca
Nick <nick.moi@gmail.com>
2019-11-19 21:22:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+    return drivers.filter(function(driver) {return driver.toUpperCase() === name.toUpperCase();});
+}
+
+function fuzzyMatch(drivers, query){
+    let result = drivers.filter(function(driver) {
+       let substring = driver.substring(0, query.length)
+       return query.toUpperCase() === substring.toUpperCase()
+    });
+
+    return result;
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name === name;
+    })
+}
